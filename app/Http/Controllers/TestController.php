b6fbd1f1c64f5e5e1257cbbe009d21bbe167bf70
naotho17 <naotho17@gmail.com>
2024-04-26 23:17:40 +0900

    contorollerでmodelを取得し,viewに渡す

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test;  //Testモデルの読み込み
 
 class TestController extends Controller
 {
     public function index()
     {
-        return view('tests.test');
+        $values = Test::all(); //全件取得
+
+        // dd($values);  //die+var_dump 処理を止めて内容を確認できる
+
+        return view('tests.test', compact('values')); //compact関数でViewに渡すと楽
     }
 }
--- resources/views/tests/test.blade.php
+++ resources/views/tests/test.blade.php
@@ -1,1 +1,6 @@
-test
+test<br>
+
+@foreach( $values as $value )
+ID:{{ $value->id }}<br>
+Text:{{ $value->text }}<br>
+@endforeach
